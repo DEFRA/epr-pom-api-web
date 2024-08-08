3503239ba26b2fab09a0bf911a3f134d6db8adc8
Mazar Shaik <mazar.shaik.external@eviden.com>
2024-08-08 14:00:28 +0100

    reverted null check here not to change existing behaviour

--- WebApiGateway/WebApiGateway.Api/Extensions/EnumExtensions.cs
+++ WebApiGateway/WebApiGateway.Api/Extensions/EnumExtensions.cs
@@ -7,12 +7,9 @@ public static class EnumExtensions
 {
     public static string GetDisplayName(this Enum enumValue)
     {
-        var memberInfo = enumValue.GetType().GetMember(enumValue.ToString());
-        if (memberInfo.Length > 0)
-        {
-            var attribute = memberInfo[0].GetCustomAttribute<DisplayAttribute>();
-            return attribute?.GetName();
-        }
-        return null;
+        return enumValue.GetType()
+            .GetMember(enumValue.ToString())[0]
+            .GetCustomAttribute<DisplayAttribute>()
+            ?.GetName();
     }
 }

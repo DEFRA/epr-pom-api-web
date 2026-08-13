094a48a6aa11b977b36120e2a571a6b30242f637
Killian Davies <killian.davies@esynergy.co.uk>
2026-08-13 14:17:09 +0100

    Feature/sub 317 move calc to payment service (#32)
    * Add regulator nation for registration submission
    
    * correction to payment service auth handler

--- WebApiGateway/WebApiGateway.Core/Models/Submission/RegistrationApplicationDetails.cs
+++ WebApiGateway/WebApiGateway.Core/Models/Submission/RegistrationApplicationDetails.cs
@@ -52,7 +52,7 @@ public class RegistrationApplicationDetails
     public RegistrationFeeCalculationDetails[]? RegistrationFeeCalculationDetails { get; set; }
 
     public string? RegistrationJourney { get; set; }
-    
+
     public class LastSubmittedFileDetails
     {
         public Guid? FileId { get; set; }
